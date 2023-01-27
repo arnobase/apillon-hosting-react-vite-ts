4d51e686ab894469009e1946a485c67fe66d96d5
CodeSandbox <support@codesandbox.io>
2023-01-27 16:18:35 +0000

    "Initial commit"

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+export function App() {
+  return (
+    <div className="font-semibold p-2">
+      <center>
+        I <span class="emoji hearts" role="img" aria-label="hearts"></span>
+         APILLON
+      </center>
+    </div>
+  );
+}
